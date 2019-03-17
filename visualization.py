379c4c21e3c551127d99425b6133b4f6db39f17f
Mehrzad <mh.morta@gmail.com>
2019-03-17 19:56:19 -0400

    Enhanhing the plots

--- visualization.py
+++ visualization.py
@@ -9,67 +9,53 @@ import argparse
 import os
 import re
 import glob
+import math
 
 def plots(train_losses, val_losses, train_ppls, val_ppls, epoch_times, experiment, directory ):
-    plt.figure(1, figsize=(16, 9))
     epochs = range(40)
     train_losses = np.array(train_losses)
     val_losses = np.array(val_losses)
     temp = len(train_losses)/40
     temp2 = len(val_losses)/40
+
+    plt.figure(1, figsize=(20, 12))
     train_losses_index = [(i % temp) == (temp-1 ) for i in range(len(train_losses))]
     val_losses_index = [(i % temp2 )== (temp2-1) for i in range(len(val_losses))]
     plt.plot(epochs, train_losses[train_losses_index], label="Train Losses (end)", marker='o' )
     plt.plot(epochs, val_losses[val_losses_index], label="Validation Losses", marker='o' )
-
-    # train_losses_index = [(i % 1327 == 0) for i in range(len(train_losses))]
-    # val_losses_index = [(i % 105 == 0) for i in range(len(val_losses))]
-    # plt.plot(epochs, train_losses[train_losses_index], label="Train Losses(beggining)")
-    # plt.scatter(epochs, train_losses[train_losses_index], label="Train Losses")
-    # plt.plot(epochs, val_losses[val_losses_index], label="Validation Losses")
-    # plt.scatter(epochs, val_losses[val_losses_index], label="Validation Losses")
     plt.xlabel("Epoch number")
     plt.ylabel("Loss value")
     plt.xticks(epochs,rotation='vertical')
-
-    plt.suptitle(experiment+ '\n' + "Training and Validation losses at the end of each epoch", fontsize=10)
+    plt.suptitle(experiment+ '\n' + "Training and Validation losses at the end of each epoch", fontsize=13)
     plt.legend()
-    plt.savefig(directory+'/1.png')
+    plt.savefig(directory+'/losees.png')
+    plt.close()
 
-    plt.figure(2, figsize=(16, 9))
-    plt.plot(epoch_times, train_ppls, label="Train PPL", marker='o' )
-    plt.plot(epoch_times, val_ppls, label="Validation PPL" , marker='o' )
+    plt.figure(2, figsize=(20, 12))
+    plt.plot(epoch_times, np.log(train_ppls), label="Train PPL", marker='o' )
+    plt.plot(epoch_times, np.log(val_ppls), label="Validation PPL" , marker='o' )
     plt.xlabel("Wall-clock-time")
     plt.xticks(epoch_times,rotation='vertical')
     plt.ylabel("PPL value")
-    plt.suptitle(experiment + '\n' + 'PPL over wall-clock-time', fontsize=10)
+    plt.suptitle(experiment + '\n' + 'log(PPL) w.r.t the wall-clock-time', fontsize=13)
     plt.legend()
-    plt.savefig(directory+'/2.png')
+    plt.savefig(directory+'/PPL_wrt_eopch#.png')
+    plt.close()
 
 
-    plt.figure(3, figsize=(16, 9))
-    plt.plot(epochs, train_ppls, label="Train PPL", marker='o' )
-    plt.plot(epochs, val_ppls, label="Validation PPL", marker='o' )
+    plt.figure(3, figsize=(20, 12))
+    plt.plot(epochs, np.log(train_ppls), label="Train PPL", marker='o' )
+    plt.plot(epochs, np.log(val_ppls), label="Validation PPL", marker='o' )
     plt.xlabel("Wall-clock-time")
     plt.xticks(epochs,rotation='vertical')
     plt.ylabel("PPL value")
-    plt.suptitle(experiment + '\n' + 'PPL over epcoh#', fontsize=10)
+    plt.suptitle(experiment + '\n' + 'log(PPL) w.r.t the epcoh number', fontsize=13)
     plt.legend()
-    plt.savefig(directory+'/3.png')
+    plt.savefig(directory+'/PPL_wrt_wct.png')
     # plt.show()
+    plt.close()
 
 def parse_args(directory):
-    # parser = argparse.ArgumentParser(description='Visualization Argument Parser')
-
-    # parser.add_argument('--save_dir', type=str, default='',
-    #                     help='path to save the experimental config, logs, model \
-    #                     This is automatically generated based on the command line \
-    #                     arguments you pass and only needs to be set if you want a \
-    #                     custom dir name')
-
-    # args = parser.parse_args()
-    # argsdict = args.__dict__
-
     lc_path  = os.path.join(directory, 'learning_curves.npy')
     log_path = os.path.join(directory, 'log.txt')
     experiment = str.split(directory, "/")[1]
